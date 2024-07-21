10ecd1965f00f21799964e8a540c476dee05fb5e
Jose Manuel Cuervo Muñoz <jocuervo@bancolombia.com.co>
2024-07-21 20:08:07 +0200

    chore: Update npm dependencies for @auth0/auth0-angular to version 2.2.3

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,19 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+//Auth0
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAuth0({
+      domain: 'jcuervom.eu.auth0.com',
+      clientId: 'cX7DLjRB0PVHdFDCgSrvRXDgRudyEx30',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+  ],
 };
